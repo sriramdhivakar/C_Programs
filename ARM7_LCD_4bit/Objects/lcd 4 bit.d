dddae74e8c95d79845afbb50f452faef22034d9e
sriram <sriram@phytec.in>
2019-12-06 09:37:00 +0530

    added ARM7_LCD_4bit folder

---
+++ ARM7_LCD_4bit/Objects/lcd 4 bit.d
@@ -0,0 +1,5 @@
+.\objects\lcd 4 bit.o: LCD 4 bit.c
+.\objects\lcd 4 bit.o: C:\Keil_v5\ARM\Inc\Philips\lpc214x.h
+.\objects\lcd 4 bit.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\lcd 4 bit.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\lcd 4 bit.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
