c69567c95984814ddfd8af650d38e8d463da9b83
sriram <sriram@phytec.in>
2019-12-06 13:47:30 +0530

    changes done i.e add new line in c/io/datastructure/struct3.c

--- c/io/datastructure/struct3.c
+++ c/io/datastructure/struct3.c
@@ -13,13 +13,16 @@ struct student{
 };
 int main(void){
 
-	struct student sriram[2] = {
+	struct student sriram[3] = {
 		{"sriram",100,29},
+		{"angu",101,19},
 		{"angu",101,19}
 	};
 
 	printf("%s,%d,%d\n",sriram[0].name,sriram[0].roll_no,sriram[0].age);
 
 	printf("%s,%d,%d\n",sriram[1].name,sriram[1].roll_no,sriram[1].age);
+	
+	printf("%s,%d,%d\n",sriram[2].name,sriram[2].roll_no,sriram[2].age);
 	return 0;
 }
