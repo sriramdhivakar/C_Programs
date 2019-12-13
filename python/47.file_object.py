59d9372b6c313d28823c04a268b998f38180b3bc
sriram <sriram@phytec.in>
2019-12-13 10:48:46 +0530

    added EmbeddedLinuxBBB-master.zip in this branch

--- python/47.file_object.py
+++ python/47.file_object.py
@@ -6,3 +6,5 @@ print ("Name of the file: ", fo.name)
 print ("Closed or not : ", fo.closed)
 print ("Opening mode : ", fo.mode)
 print ("Softspace flag : ", fo.softspace)
+
+fo.close()
--- python/48.file_write.py
+++ python/48.file_write.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 # Open a file
-fo = open("foo.txt", "wb")
+fo = open("foo.txt", "w")
 fo.write( "Python is a great language.\nYeah its great!!\n")
 # Close opend file
-fo.close()
